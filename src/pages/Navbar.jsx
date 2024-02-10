da6fd2d7e4979934aef967350737a0a33d8a6bfe
ruthvikthimmoji <ruthvikthimmoji@gmail.com>
2024-02-10 16:28:17 +0530

    initial commits

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+    <div>
+        <h3>{Date.now}</h3>
+    </div>
+    <div>
+
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar
